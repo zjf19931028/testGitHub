6799c798da51fdb6570be011c44e48e27a1f1d04
zhangjingfang <zhangjingfang@jeejio.com>
2022-06-30 15:20:26 +0800

    Feat:20220630 15:17

--- app/src/main/java/com/awesome/test/MainActivity.kt
+++ app/src/main/java/com/awesome/test/MainActivity.kt
@@ -2,6 +2,7 @@ package com.awesome.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.awesome.anew.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
